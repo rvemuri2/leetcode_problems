d9bb71fbb871e5af4fb528e4fa2b929d3dd0c25e
rvemuri2 <rohitvemuri@yahoo.com>
2024-10-12 22:48:14 -0700

    Solving daily temperatures

---
+++ stacks/3-daily_temperatures/solution.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+        for r in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[r]:
+                l = stack.pop()
+                answer[l] = r-l
+
+            stack.append(r)
+
+        return answer
