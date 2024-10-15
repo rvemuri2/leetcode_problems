ddcaafb68334a8e4168426b32cef3e2026f90333
rvemuri2 <rohitvemuri@yahoo.com>
2024-10-15 15:46:24 -0700

    added another solution to longest character replacement

--- sliding_window/6-longest_repeating_character_replacement/solution2.py
+++ sliding_window/6-longest_repeating_character_replacement/solution2.py
@@ -4,12 +4,14 @@ class Solution:
         l = 0
         h = {}
         res = 0
+        maxf = 0
 
         for i in range(len(s)):
 
             h[s[i]] = h.get(s[i], 0) + 1
+            maxf = max(maxf, h[s[i]])
 
-            if((i - l + 1) - max(h.values()) > k):
+            if((i - l + 1) - maxf > k):
                 h[s[l]] = h.get(s[l]) - 1
                 l += 1 
                 
