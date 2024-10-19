c7f2b6acd15be6bed099accdf3c695f150e34f60
rvemuri2 <rohitvemuri@yahoo.com>
2024-10-18 18:34:21 -0700

    solved max number of vowels

---
+++ sliding_window/7-max_number_of_vowels_in_substring/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        l = 0
+        res = 0
+        a = ["a", "e", "i", "o", "u"]
+        count = 0
+
+        for i in range(len(s)):
+
+            if(s[i] in a):
+                count += 1
+
+            if(i - l + 1 > k):
+                if(s[l] in a):
+                    count -= 1
+                
+                l += 1
+            
+            res = max(res, count)
+        return res
